2494489cf35a418ce385be9baaf61013ec79e4f8
LieStage <118051769+LieStage@users.noreply.github.com>
2024-11-15 15:28:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Pokemondb:dbpokemon@cluster0.ecjoh.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://misoc51233:f3RsoxIbeln4qBQF@cluster0.z4zu0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "telegramfiles")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
