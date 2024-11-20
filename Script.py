14d721f7022dd1177406bee52410b21189e042a6
LieStage <118051769+LieStage@users.noreply.github.com>
2024-11-20 12:48:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -93,16 +93,16 @@ This module only works for my admins
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
     STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-🌇𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂    : <code>{}</code>
-🗼𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂    : <code>{}</code>
-🗽𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴   : <code>{}</code> 𝙼𝚒𝙱
-🏙️𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴   : <code>{}</code> 𝙼𝚒𝙱"""
-    LOG_TEXT_G = """#NewGroup
+𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂    : <code>{}</code>
+𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂    : <code>{}</code>
+𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴   : <code>{}</code> 𝙼𝚒𝙱
+𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴   : <code>{}</code> 𝙼𝚒𝙱"""
+    LOG_TEXT_G = """#NewGroup(ULTRAV2)
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
 Added By - {}
 """
-    LOG_TEXT_P = """#NewUserforTeamTb
+    LOG_TEXT_P = """#NewUserforULTRAV2
 ID - <code>{}</code>
 Name - {}
 """
